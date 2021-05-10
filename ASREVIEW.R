81f57de3765666ab04d7602165902a0f1aedf979
Lissa <c.j.vanlissa@uu.nl>
2021-05-10 18:37:48 +0200

    Final submission

--- ASREVIEW.R
+++ ASREVIEW.R
@@ -3,6 +3,6 @@ library(reticulate)
 use_condaenv("veni_sysrev") #C:\Users\lissa102\AppData\Local\Continuum\miniconda3\envs\veni_sysrev
 asreview <- import("asreview")
 asreview
-asreview oracle YOUR_DATA.csv --state_file myreview.h5
+#asreview oracle YOUR_DATA.csv --state_file myreview.h5
 
 py_run_string("asreview oracle")
